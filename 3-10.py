dbd3a90390a48f97a9537faf0c5bf7ad7608eb96
0itsme0 <44812083+0itsme0@users.noreply.github.com>
2022-09-09 19:40:36 +0300

    Update 3-10.py

--- 3-10.py
+++ 3-10.py
@@ -1,21 +1,21 @@
 
-cities = ['vorkuta', 'inta', 'pechora', 'ukhta']
+cities = ['vorkuta', 'inta', 'pechora', 'ukhta'] # соЗДАЕМ СПИСОК
+print(cities) # ВЫВОДИМ НА ЭКРАН
+
+print(cities[3]) # ВЫВОДИМ КОНКРЕТНОЕ ЗНАЧЕНИЕ ПО ИНДЕКСАЦИИ
+
+message = "Я живу в " + cities[0] + "." # ВЫЧЛЕНЯЕМ ОПРЕДЕЛЕННОЕ ЗНАЧЕНИЕ СПИСКА
+
+cities.append('syktyvkar') # ДОБАВЛЯЕМ К КОНЦУ СПИСКА ГОРОД
 print(cities)
 
-print(cities[3])
-
-message = "Я живу в " + cities[0] + "."
-
-cities.append('syktyvkar')
+cities.remove('vorkuta') # УБИРАЕМ ОПРЕДЕЛЕННОЕ ЗНАЧЕНИЕ
 print(cities)
 
-cities.remove('vorkuta')
+cities.sort() # СОРТИРУЕМ ПО АЛФАФИТНОМУ ПОРЯДКУ
 print(cities)
 
-cities.sort()
+cities.reverse() # ПЕРЕМЕШИВАЕМ СПИСОК
 print(cities)
 
-cities.reverse()
-print(cities)
-
-print(len(cities))
+print(len(cities)) # СЧИТАЕМ КОЛИЧЕСТВО ГОРОДОВ
