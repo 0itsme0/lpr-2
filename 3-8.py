f3cda1fdb4a2da90bfb5f5f1c8905427e4b17410
0itsme0 <44812083+0itsme0@users.noreply.github.com>
2022-09-09 19:37:01 +0300

    Update 3-8.py

--- 3-8.py
+++ 3-8.py
@@ -1,12 +1,12 @@
-spisok = ['USA', 'Ukraine', 'Afrika', 'Kazahstan', 'Afganistan']
-print(spisok)
-print(sorted(spisok))
-print(spisok)
-spisok.reverse()
-print(spisok)
-spisok.reverse()
-print(spisok)
-spisok.sort()
-print(spisok)
-spisok.reverse()
-print(spisok)
+spisok = ['USA', 'Ukraine', 'Afrika', 'Kazahstan', 'Afganistan'] # Задаем переменную со списком стран
+print(spisok) # Выводим этот список так, как видит Пайтон
+print(sorted(spisok)) # Временно сортируем этот список в алфавитном порядке
+print(spisok) # Показываем, что список никак не поменялся
+spisok.reverse() # Перемешиваем наш список
+print(spisok) # Смотрим, что список изменился
+spisok.reverse() # Снова перемешиваем список
+print(spisok) # Смотрим, что список изменился
+spisok.sort() # Сортирую на постоянную основу
+print(spisok) # Выводим сортированный список
+spisok.reverse() # Выводим список в обратном порядке
+print(spisok) # Смотрим на список
